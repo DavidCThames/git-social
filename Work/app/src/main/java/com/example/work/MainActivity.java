11dc7e291a209194a65eb7bd8dc880afd970cf8d
jackschu <js9pa@virginia.edu>
2019-02-16 14:33:22 -0500

    adding get image string from url

--- Work/app/src/main/java/com/example/work/MainActivity.java
+++ Work/app/src/main/java/com/example/work/MainActivity.java
@@ -60,6 +60,29 @@ public class MainActivity extends AppCompatActivity {
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
         byte[] imageBytes = baos.toByteArray();
         String imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+	// Instantiate the RequestQueue.
+	RequestQueue queue = Volley.newRequestQueue(this);
+	String url ="http://git-social.com/api/v1/image";
+	
+	// Request a string response from the provided URL.
+	StringRequest stringRequest = new StringRequest(Request.Method.GET, url,new Response.Listener<String>() {
+		@Override
+		public void onResponse(String response) {
+		    // Display the first 500 characters of the response string.
+		    //		    mTextView.setText("Response is: "+ response.substring(0,500));
+		    imageString = response
+		}
+	    }, new Response.ErrorListener() {
+		    @Override
+		    public void onErrorResponse(VolleyError error) {
+			mTextView.setText("That didn't work!");
+		    }
+		});
+
+	// Add the request to the RequestQueue.
+	queue.add(stringRequest);
+
+
 
         Bitmap decodedImage = decodeBase64(imageString);
 

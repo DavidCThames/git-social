c7286380d5bf4395f8ff01dff883ec9b5bd889a6
jackschu <js9pa@virginia.edu>
2019-02-16 17:31:52 -0500

    setting up messages

--- DjangoAPI/gitsocial/gitsocial/settings.py
+++ DjangoAPI/gitsocial/gitsocial/settings.py
@@ -121,3 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+ 
+]
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'hello.studyskills@gmail.com' 
+EMAIL_HOST_PASSWORD = 'caxmczsuwwglfmqr'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True

68cd7bc36a40dc069d85c35d1a1f833349774d51
jackschu <js9pa@virginia.edu>
2019-02-16 00:02:08 -0500

    updated custom user object

--- DjangoAPI/gitsocial/gitsocial/settings.py
+++ DjangoAPI/gitsocial/gitsocial/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+#    'gitapi.apps.GitapiConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -83,7 +85,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = 'users.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',

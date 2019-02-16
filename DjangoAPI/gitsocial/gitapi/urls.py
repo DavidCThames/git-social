5e724fa4bb6923f2d9debe4642eb845be319af61
jackschu <js9pa@virginia.edu>
2019-02-16 13:11:10 -0500

    added dashes in url and pillow

--- DjangoAPI/gitsocial/gitapi/urls.py
+++ DjangoAPI/gitsocial/gitapi/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from . import api
 
 urlpatterns = [
-    url(r'^api/v1/(?P<owner>\w+)/(?P<repo>\w+)/user/(?P<username>\w+)/lines/week/?$', api.get_lines_week, name="get_lines_week"),
-    url(r'^api/v1/(?P<owner>\w+)/(?P<repo>\w+)/user/(?P<username>\w+)/commits/week/?$', api.get_commits_week, name="get_lines_week"),
-    url(r'^api/v1/(?P<owner>\w+)/(?P<repo>\w+)/leaderboard/commits/week/?$', api.get_leaderboard_commits_week, name="get_commits_week"),
+    url(r'^api/v1/(?P<owner>[\w-]+)/(?P<repo>[\w-]+)/user/(?P<username>[\w-]+)/lines/week/?$', api.get_lines_week, name="get_lines_week"),
+    url(r'^api/v1/(?P<owner>[\w-]+)/(?P<repo>[\w-]+)/user/(?P<username>[\w-]+)/commits/week/?$', api.get_commits_week, name="get_lines_week"),
+    url(r'^api/v1/(?P<owner>[\w-]+)/(?P<repo>[\w-]+)/leaderboard/commits/week/?$', api.get_leaderboard_commits_week, name="get_commits_week"),
     url(r'^api/v1/image/?$', api.get_image, name="get_image"),
 ]

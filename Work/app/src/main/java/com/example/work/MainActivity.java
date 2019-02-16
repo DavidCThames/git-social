223c9076846d9c63bf52cc9beb9e90d88f9fdfd2
hmir <hamzasmir@gmail.com>
2019-02-16 14:17:29 -0500

    fixed type in MainActivity.java

--- Work/app/src/main/java/com/example/work/MainActivity.java
+++ Work/app/src/main/java/com/example/work/MainActivity.java
@@ -26,7 +26,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;å
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 

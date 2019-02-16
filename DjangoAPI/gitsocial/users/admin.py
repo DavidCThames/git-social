432a93f214f7ecc9f21a59c1201994876aa8097d
jackschu <js9pa@virginia.edu>
2019-02-16 00:12:48 -0500

    adding repos to admin view

--- DjangoAPI/gitsocial/users/admin.py
+++ DjangoAPI/gitsocial/users/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from django.contrib.auth.forms import UserChangeForm
+from . models import User
 
-admin.site.register(User, UserAdmin)
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+class MyUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': ('repos',)}),
+    )
+
+
+admin.site.register(User, MyUserAdmin)

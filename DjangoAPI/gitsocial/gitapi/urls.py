8b2014082a5cf986e616859a9a51a58c489dfa92
David Thames <davidct@vt.edu>
2019-02-16 03:59:42 -0500

    Added sample image served with base64 encoding

--- DjangoAPI/gitsocial/gitapi/urls.py
+++ DjangoAPI/gitsocial/gitapi/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^api/v1/(?P<owner>\w+)/(?P<repo>\w+)/user/(?P<username>\w+)/lines/week/?$', api.get_lines_week, name="get_lines_week"),
     url(r'^api/v1/(?P<owner>\w+)/(?P<repo>\w+)/user/(?P<username>\w+)/commits/week/?$', api.get_commits_week, name="get_lines_week"),
     url(r'^api/v1/(?P<owner>\w+)/(?P<repo>\w+)/leaderboard/commits/week/?$', api.get_leaderboard_commits_week, name="get_commits_week"),
+    url(r'^api/v1/image/?$', api.get_image, name="get_image"),
 ]

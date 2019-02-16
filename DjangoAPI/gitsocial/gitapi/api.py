a13dd98f43deb89cea8e95d01152f1aa5358c490
David Thames <davidct@vt.edu>
2019-02-16 00:41:58 -0500

    Added week leaderboard based on commits

--- DjangoAPI/gitsocial/gitapi/api.py
+++ DjangoAPI/gitsocial/gitapi/api.py
@@ -5,10 +5,11 @@ g = Github('ebd026d4736c985826055cc7b1d8a5db1c6f26b3')
 
 def contributor_to_dict(contributor):
     d = {}
+    d['username'] = contributor.author.login
     d['name'] = contributor.author.name
     d['total'] = contributor.total
     last_week = contributor.weeks[0]
-    d['last_week'] = {
+    d['last_week'] = { # TODO: Might be first week...
         
         'additions': last_week.a,
         'deletes' : last_week.d,
@@ -46,5 +47,21 @@ def get_commits_week(request, owner, repo, username):
         json['success'] = False
         json['error']  = 'username not found'
             
+
+    return JsonResponse(json, safe=False)
+
+def get_leaderboard_commits_week(request, owner, repo):
+    repo = g.get_repo(owner + '/' + repo)
+    stats = repo.get_stats_contributors()
+    json = {'success': True, 'contributors': []}
+    for i, contributor in enumerate(stats): #Convert author objects to the username
+        contributor_dict = contributor_to_dict(contributor)
+        contributor_json = {}
+        contributor_json['username'] = contributor_dict['username']
+        contributor_json['name'] = contributor_dict['name']
+        contributor_json['commits'] = contributor_dict['last_week']['commits']
+        json['contributors'].append(contributor_json)
+
+    json['contributors'].sort(key=lambda x: x['commits'], reverse=True)
 
     return JsonResponse(json, safe=False)
--- DjangoAPI/gitsocial/gitapi/urls.py
+++ DjangoAPI/gitsocial/gitapi/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from . import api
 
 urlpatterns = [
-    url(r'^api/v1/(?P<owner>\w+)/(?P<repo>\w+)/(?P<username>\w+)/lines/week/?$', api.get_lines_week, name="get_lines_week"),
-    url(r'^api/v1/(?P<owner>\w+)/(?P<repo>\w+)/(?P<username>\w+)/commits/week/?$', api.get_commits_week, name="get_lines_week"),
+    url(r'^api/v1/(?P<owner>\w+)/(?P<repo>\w+)/user/(?P<username>\w+)/lines/week/?$', api.get_lines_week, name="get_lines_week"),
+    url(r'^api/v1/(?P<owner>\w+)/(?P<repo>\w+)/user/(?P<username>\w+)/commits/week/?$', api.get_commits_week, name="get_lines_week"),
+    url(r'^api/v1/(?P<owner>\w+)/(?P<repo>\w+)/leaderboard/commits/week/?$', api.get_leaderboard_commits_week, name="get_commits_week"),
 ]

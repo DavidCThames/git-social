47c41c90c2f445a1aff2d3f2128be8b841b0cb85
jackschu <js9pa@virginia.edu>
2019-02-17 00:56:49 -0500

    add repo to user is functional

--- DjangoAPI/gitsocial/repos/models.py
+++ DjangoAPI/gitsocial/repos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Repos(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.CharField(max_length=200)
+    repo_name = models.CharField(max_length=200)
+
--- DjangoAPI/gitsocial/users/models.py
+++ DjangoAPI/gitsocial/users/models.py
@@ -4,10 +4,4 @@ import json
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    repos = models.CharField(max_length=500)
-    
-    def set_repos(self, x):
-        self.repos = json.dumps(x)
-
-    def get_repos(self):
-        return json.loads(self.repos)
+    pass

13f91dcf4de72b9a8e83666eb1492d7a1979ef9d
robleshs <robleshs@vt.edu>
2019-02-16 23:43:30 -0500

    Initial design for login page

--- GitSocial/LoginPage.js
+++ GitSocial/LoginPage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
+import { TextInput, Button, Surface } from 'react-native-paper';
 
 export default class LoginPage extends Component {
     static navigationOptions = {
@@ -29,29 +29,54 @@ export default class LoginPage extends Component {
     render() {
         return (
             <View style={styles.app_container}>
-                <View style={styles.text_container}> 
-                    <Text style={styles.commit_text}> Login Page </Text> 
-                </View>
+                
+                {/*<Title style={{fontSize: 40, backgroundColor: '#6200EE', padding: 25, color: '#ffffff'}}>
+                    Git Social
+                </Title>*/}
+                <Surface style={styles.surface}>
+                    <Text style={{fontFamily: 'roboto', fontSize: 60, padding: 10, color: '#ffffff'}}>
+                        Git Social
+                    </Text>
+                </Surface>
 
+                <View style={{height: 150}}></View>
+
+                <View style={styles.text_container}> 
+                    <Text style={styles.commit_text}>Sign In With GitHub</Text> 
+                </View>
+                
                 <View style={styles.login_container}>
                     <TextInput
                         mode="outlined"
                         label="Email"
-                        value={this.state.text}
-                        onChangeText={text => this.setState({ text })}
+                        value={this.state.loginText}
+                        onChangeText={loginText => this.setState({ loginText })}
                     />
                 </View>
                 
+                <View style={styles.login_container}>
+                    <TextInput
+                        mode="outlined"
+                        label="Password"
+                        value={this.state.passwordText}
+                        onChangeText={passwordText => this.setState({ passwordText })}
+                    />
+                </View>
+                
+                <View style={{height: 20}}></View>
 
                 <View style={styles.loginbutton_container}>
                     <Button 
                         loading={this.state.loggingIn} 
                         mode="contained" 
-                        onPress={() => this.setState({loggingIn: true})}
-                        style={{width: 200}}
-                    >
+                        onPress={() => this.login()}
+                        style={{justifyContent: 'center',
+                        alignItems: 'center', width: 200, height: 75, color: '#6200EE'}}>
 
-                        Login
+                        <Text style={{fontSize: 25}}>
+                          LOG IN
+                        </Text>
+
                     </Button>
                 </View>
 
@@ -60,15 +85,16 @@ export default class LoginPage extends Component {
     }
   }
   
+
 const styles = StyleSheet.create({
     app_container: {
       flex: 1,
-      justifyContent: 'center',
+      //justifyContent: 'center',
       alignItems: 'center',
       backgroundColor: '#F5FCFF',
     },
     commit_text: {
-      fontSize: 20,
+      fontSize: 25,
       textAlign: 'center',
       margin: 10,
     },
@@ -77,8 +103,18 @@ const styles = StyleSheet.create({
       color: '#333333',
       marginBottom: 5,
     },
+    surface: {
+      padding: 20,
+      height: 150,
+      width: 600,
+      top: 0,
+      backgroundColor: '#6200EE',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      elevation: 4,
+    },
     login_container: {
-        width: 200,
+        width: 250,
         marginBottom: 10,
     }
 });

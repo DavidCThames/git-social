3ea99c54d5b7a88db9b36d1dfb35e6ea500f8032
David Thames <davidct@vt.edu>
2019-02-17 02:38:11 -0500

    Added badges and implemented switching between months and weeks

--- DjangoAPI/gitsocial/webapp/urls.py
+++ DjangoAPI/gitsocial/webapp/urls.py
@@ -6,5 +6,5 @@ app_name = 'webapp'
 urlpatterns = [
     # url(r'^apidocs/?$',views.apidocs, name="apidocs"),
     url('home',views.home, name="index"),
-    url(r'^(?P<owner>[\w-]+)/(?P<repo>[\w-]+)/user/(?P<username>[\w-]+)/?$', views.user_info, name="user_info"),
-    ]
+    url(r'^(?P<owner>[\w-]+)/(?P<repo>[\w-]+)/user/(?P<username>[\w-]+)/(?P<time>[\w-]+)/?$', views.user_info, name="user_info"),
+]
--- DjangoAPI/gitsocial/webapp/views.py
+++ DjangoAPI/gitsocial/webapp/views.py
@@ -5,22 +5,34 @@ import requests
 def home(request):
     return render(request, 'webapp/home.html')
 
-def user_info(request, owner, repo, username):
+def user_info(request, owner, repo, username, time):
 
-    api_endpoint = 'http://git-social.com/api/v1/'+ owner + '/' + repo + '/user/' + username + '/lines/week/'
+    api_endpoint = 'http://git-social.com/api/v1/'+ owner + '/' + repo + '/user/' + username + '/lines/' + time + '/'
     response = requests.get(api_endpoint)
     json_response = response.json()
     data_lines = json_response['lines']
 
-    api_endpoint = 'http://git-social.com/api/v1/'+ owner + '/' + repo + '/user/' + username + '/commits/week/'
+    api_endpoint = 'http://git-social.com/api/v1/'+ owner + '/' + repo + '/user/' + username + '/commits/' + time + '/'
     response = requests.get(api_endpoint)
     json_response = response.json()
     data_commits = json_response['commits']
 
-    api_endpoint = 'http://git-social.com/api/v1/'+ owner + '/' + repo + '/leaderboard/commits/week'
+    api_endpoint = 'http://git-social.com/api/v1/'+ owner + '/' + repo + '/user/' + username + '/badges/' + time + '/'
+    response = requests.get(api_endpoint)
+    json_response = response.json()
+    badge_ids = json_response['badges']
+
+    data_badges = []
+    for badge_id in badge_ids:
+        api_endpoint = 'http://git-social.com/api/v1/badge/0' + str(badge_id)
+        response = requests.get(api_endpoint)
+        data_badges.append(response.text)
+    
+    api_endpoint = 'http://git-social.com/api/v1/'+ owner + '/' + repo + '/leaderboard/commits/' + time + '/'
     response = requests.get(api_endpoint)
     json_response = response.json()
     data_leaderboard = json_response['contributors'][:10]
+    
 
 
-    return render(request, 'webapp/user_info.html', {'owner': owner, 'repo': repo, 'username': username, 'data_lines': data_lines, 'data_commits': data_commits, 'data_leaderboard': data_leaderboard})
+    return render(request, 'webapp/user_info.html', {'owner': owner, 'repo': repo, 'username': username, 'data_lines': data_lines, 'data_commits': data_commits, 'data_badges': data_badges, 'data_leaderboard': data_leaderboard})

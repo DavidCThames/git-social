d8d4d2e5cf37dbe7b0d38e9b81e3c9188cf12ca9
jackschu <js9pa@virginia.edu>
2019-02-17 01:40:43 -0500

    adding dropdown

--- DjangoAPI/gitsocial/webapp/urls.py
+++ DjangoAPI/gitsocial/webapp/urls.py
@@ -6,5 +6,5 @@ app_name = 'webapp'
 urlpatterns = [
     # url(r'^apidocs/?$',views.apidocs, name="apidocs"),
     url('home',views.home, name="index"),
-    url(r'^(?P<owner>[\w-]+)/(?P<repo>[\w-]+)/user/(?P<username>[\w-]+)/?$', views.user_info, name="get_lines_week"),
+    url(r'^(?P<owner>[\w-]+)/(?P<repo>[\w-]+)/user/(?P<username>[\w-]+)/?$', views.user_info, name="user_info"),
     ]

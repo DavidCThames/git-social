ca65924fa1ec17afdc8b782959b70f325c936929
hmir <hamzasmir@gmail.com>
2019-02-17 08:50:26 -0500

    added highlighting to leaderboard

--- GitSocial/Leaderboard.js
+++ GitSocial/Leaderboard.js
@@ -5,6 +5,29 @@ import { DataTable } from 'react-native-paper';
 export default class Leaderboard extends Component {
     constructor(props) {
       super(props);
+
+      this.renderRow = this.renderRow.bind(this);
+    }
+
+    renderRow(rowData, index) {
+        if (rowData.username.trim() === global.user.trim()) {
+            return (
+                <DataTable.Row style={{backgroundColor:"#faecc2"}}>
+                <DataTable.Cell>{index + 1}</DataTable.Cell>
+                <DataTable.Cell>{rowData.username}</DataTable.Cell>
+                <DataTable.Cell numeric>{rowData.commits}</DataTable.Cell>
+                </DataTable.Row>
+            );
+        }
+        else {
+            return (
+                <DataTable.Row>
+                <DataTable.Cell>{index + 1}</DataTable.Cell>
+                <DataTable.Cell>{rowData.username}</DataTable.Cell>
+                <DataTable.Cell numeric>{rowData.commits}</DataTable.Cell>
+                </DataTable.Row>
+            );
+        }
     }
     
     render() {
@@ -12,16 +35,12 @@ export default class Leaderboard extends Component {
             <ScrollView>
                 <DataTable>
                 <DataTable.Header>
+                <DataTable.Title>Rank</DataTable.Title>
                 <DataTable.Title>Username</DataTable.Title>
                 <DataTable.Title numeric>Commits</DataTable.Title>
                 </DataTable.Header>
                 {
-                    this.props.arr.map((rowData, index) => (
-                        <DataTable.Row>
-                        <DataTable.Cell>{rowData.username}</DataTable.Cell>
-                        <DataTable.Cell numeric>{rowData.commits}</DataTable.Cell>
-                        </DataTable.Row>
-                    ))
+                    this.props.arr.map(this.renderRow)
                 }
                 </DataTable>
             </ScrollView>

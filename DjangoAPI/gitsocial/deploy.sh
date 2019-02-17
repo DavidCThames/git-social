94e0f72d94eda40edcef0b95dd87d7d10a54b9a7
jackschu <js9pa@virginia.edu>
2019-02-17 04:22:39 -0500

    adding deploy script

---
+++ DjangoAPI/gitsocial/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+exec docker build -t temp .
+exec docker tag temp:latest gcr.io/kube-231906/vthacks_back:latest
+exec docker push gcr.io/kube-231906/vthacks_back:latest
+exec kubectl patch deployment vt-back -p  '{"spec":{"template":{"spec":{"terminationGracePeriodSeconds":31}}}}'
+exec kubectl patch deployment vt-back -p  '{"spec":{"template":{"spec":{"terminationGracePeriodSeconds":30}}}}'
+echo 'done'

338f4bb0abc2bf62efd983477d7e5405d1ddda2f
David Thames <davidct@vt.edu>
2019-02-17 00:06:17 -0500

    Allowed viewing a username on a repo through the web app

--- DjangoAPI/gitsocial/webapp/views.py
+++ DjangoAPI/gitsocial/webapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'webapp/home.html')
+
+def user_info(request, owner, repo, username):
+    return render(request, 'webapp/user_info.html', {'owner': owner, 'repo': repo, 'username': username})
